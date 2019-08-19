09d2364bc6245db830603145876d6918903ac44e
Jonas Gilg <jonas.gilg@dlr.de>
2019-08-19 15:10:50 +0200

    :hammer: UI now works with Bootstrap instead of Materialize.

--- gui/js/lod_body_settings.js
+++ gui/js/lod_body_settings.js
@@ -1,7 +1,7 @@
 init_slider("set_terrain_height", 1.0, 20.0, 0.1, [1]);
 init_slider("set_height_range", -12.0, 21.0, 0.1, [-8, 12]);
 init_slider("set_slope_range", 0.0, 90.0, 1.0, [0, 45]);
-init_slider("set_terrain_lod", 15.0, 50.0, 0.1, [15]);
+init_slider("set_terrain_lod", 10.0, 50.0, 0.1, [15]);
 init_slider("set_texture_gamma", 0.1, 3.0, 0.01, [1.0]);
 
 
@@ -11,3 +11,12 @@ $('#set_enable_auto_terrain_lod').change(function () {
     else
         $("#set_terrain_lod").removeClass("unresponsive");
 });
+
+
+function set_map_data_copyright(text) {
+    $("#img-data-copyright").tooltip({title: "© " + text, placement: "top"});
+}
+
+function set_elevation_data_copyright(text) {
+    $("#dem-data-copyright").tooltip({title: "© " + text, placement: "bottom"});
+}
